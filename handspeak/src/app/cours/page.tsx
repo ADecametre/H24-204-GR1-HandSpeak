7008d06b3cb53568ccb99bd5abd648849315b338
Adam4718 <adamibrahim.wk@gmail.com>
2024-04-20 17:01:28 -0400

    Progrès sur la barre de navigation (version mobile à finir)
    Changement du style de la barre, ajout d'un navbar pour la version mobile, encore largement incomplet, je vais le finaliser dans le prochain commit.

---
+++ handspeak/src/app/cours/page.tsx
@@ -0,0 +1,8 @@
+import { Text } from "@mantine/core";
+export default function Home() {
+	return (
+		<>
+			<Text>LISTE DES COURS DISPONIBLES</Text>
+		</>
+	);
+}
--- handspeak/src/components/HeaderMenu.tsx
+++ handspeak/src/components/HeaderMenu.tsx
@@ -21,6 +21,7 @@ import {
 	rem,
 	useMantineTheme,
 	AppShellHeader,
+	AppShellNavbar,
 } from "@mantine/core";
 //Fonction de la composante exportée
 export default function Name() {
@@ -28,19 +29,35 @@ export default function Name() {
 		<AppShellHeader className="sticky">
 			<Group justify="space-between" h="100%" ml="40" mr="40">
 				<Group>
-					<Image src="/Logo.png" width={75} height={75} alt="Logo HandSpeak" />
-					<Text>HandSpeak</Text>
+					<Image src="/Logo.png" width={85} height={85} alt="Logo HandSpeak" />
+					<Text
+						size="xl"
+						fw={800}
+						variant="gradient"
+						gradient={{ from: "blue", to: "purple", deg: 90 }}
+						pb={10}
+					>
+						HANDSPEAK
+					</Text>
 				</Group>
 
 				<Group h="100%" gap={100} visibleFrom="sm">
 					<Link href="/">Acceuil</Link>
-					<Link href="/test-camera">Cours</Link>
+					<Link href="/cours">Cours</Link>
 					<Link href="">À Propos</Link>
 				</Group>
-				<Group>
-					<Button variant="filled">Inscrivez vous</Button>
-					<Button variant="default">Connection</Button>
+				<Group visibleFrom="sm">
+					<Button variant="filled">Inscription</Button>
+					<Button variant="default">Connexion</Button>
 				</Group>
+				<Burger hiddenFrom="sm" />
+				<AppShellNavbar py="md" px={4}>
+					<UnstyledButton ml="20">Acceuil</UnstyledButton>
+					<UnstyledButton ml="20">Cours</UnstyledButton>
+					<UnstyledButton ml="20">À propos</UnstyledButton>
+					<Button variant="filled">Inscription</Button>
+					<Button variant="default">Connexion</Button>
+				</AppShellNavbar>
 			</Group>
 		</AppShellHeader>
 	);
