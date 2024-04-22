2dcdcddab33d9795696d83af0424ee6fdcf0ee3a
ADecametre <66925945+ADecametre@users.noreply.github.com>
2024-04-22 12:14:59 -0400

    Réparation du code
    Configuration de Next.js pour que les données soient mises à jour à chaque 30 secondes. (Par défaut, Next.js génère les pages statiquement lors du build, donc les données ne sont pas mises à jour.)
    Ajustement du code.

--- handspeak/src/app/test-mongodb/page.tsx
+++ handspeak/src/app/test-mongodb/page.tsx
@@ -9,37 +9,37 @@ async function getPlanetes() {
 	return await planet.find({}).sort("orderFromSun");
 }
 
-export default function TestMongoDB() {
-	let planetes = getPlanetes();
+export const revalidate = 30; // Rafraîchir les données à chaque 30 secondes.
+// Cette ligne n'a aucun effet en développement, mais est essentielle en production pour que les données soient mises à jour par Next.js.
+
+export default async function TestMongoDB() {
+	let planetes = await getPlanetes();
 
 	return (
 		<main className="p-2">
 			<h1 className="text-center text-3xl p-2">Planètes</h1>
 			<div className="grid grid-cols-2">
-				{planetes.then((res) =>
-					res.map((planete: Planet) => (
-						<div key={planete._id} className="border-2 p-2 rounded-lg">
-							<center className="text-center text-lg font-bold">
-								{planete.name}
-							</center>
-							<p>
-								<u>Ordre à partir du soleil :</u>{" "}
-								{planete.orderFromSun.toString()}
-							</p>
-							<p>
-								<u>Anneaux :</u> {planete.hasRings ? "Oui" : "Non"}
-							</p>
-							<p>
-								<u>Composition de l&apos;atmosphère :</u>{" "}
-								{planete.mainAtmosphere.join(", ")}
-							</p>
-							<p>
-								<u>Température (°C) :</u>{" "}
-								{planete.surfaceTemperatureC.toString()}
-							</p>
-						</div>
-					))
-				)}
+				{planetes.map((planete: Planet) => (
+					<div key={planete._id} className="border-2 p-2 rounded-lg">
+						<center className="text-center text-lg font-bold">
+							{planete.name}
+						</center>
+						<p>
+							<u>Ordre à partir du soleil :</u>{" "}
+							{planete.orderFromSun.toString()}
+						</p>
+						<p>
+							<u>Anneaux :</u> {planete.hasRings ? "Oui" : "Non"}
+						</p>
+						<p>
+							<u>Composition de l&apos;atmosphère :</u>{" "}
+							{planete.mainAtmosphere.join(", ")}
+						</p>
+						<p>
+							<u>Température (°C) :</u> {planete.surfaceTemperatureC.toString()}
+						</p>
+					</div>
+				))}
 			</div>
 		</main>
 	);
