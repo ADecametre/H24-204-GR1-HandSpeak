a897ddc7e822e1be8df5e687cf24915ce4c8ca57
ADecametre <66925945+ADecametre@users.noreply.github.com>
2024-04-22 22:20:14 -0400

    Ajustements minimes
    Ajout de variables pour éviter les répétitions.
    Ajustement du logo (renvoie vers la page d'accueil, n'est plus sélectionnable).

--- handspeak/src/components/HeaderMenu.tsx
+++ handspeak/src/components/HeaderMenu.tsx
@@ -30,40 +30,54 @@ import {
 type HeaderMenuProps = { opened: boolean; toggle: () => void };
 
 export default function HeaderMenu({ opened, toggle }: HeaderMenuProps) {
+	const liens = (
+		<>
+			<Link href="/">Acceuil</Link>
+			<Link href="/cours">Cours</Link>
+			<Link href="">À Propos</Link>
+		</>
+	);
+	const boutons = (
+		<>
+			<Button variant="filled">Inscription</Button>
+			<Button variant="default">Connexion</Button>
+		</>
+	);
+
 	return (
 		<AppShellHeader>
 			<Group justify="space-between" px={15} py={10}>
-				<Group gap="xs">
-					<Image src="/Logo.png" width={55} height={55} alt="Logo HandSpeak" />
-					<Text
-						size="xl"
-						fw={800}
-						variant="gradient"
-						gradient={{ from: "blue", to: "purple", deg: 90 }}
-					>
-						HANDSPEAK
-					</Text>
-				</Group>
+				<Link href="/">
+					<Group gap="xs" className="select-none">
+						<Image
+							src="/Logo.png"
+							width={55}
+							height={55}
+							alt="Logo HandSpeak"
+						/>
+						<Text
+							size="xl"
+							fw={800}
+							variant="gradient"
+							gradient={{ from: "blue", to: "purple", deg: 90 }}
+						>
+							HANDSPEAK
+						</Text>
+					</Group>
+				</Link>
 
 				<Group justify="space-between" w="30vw" visibleFrom="sm">
-					<Link href="/">Acceuil</Link>
-					<Link href="/cours">Cours</Link>
-					<Link href="">À Propos</Link>
+					{liens}
 				</Group>
 				<Group gap="xs" visibleFrom="sm">
-					<Button variant="filled">Inscription</Button>
-					<Button variant="default">Connexion</Button>
+					{boutons}
 				</Group>
 				<Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
 				<AppShellAside py="md" px={4}>
 					<Stack justify="space-around" gap="xl" align="center">
-						<UnstyledButton>Acceuil</UnstyledButton>
-						<UnstyledButton>Cours</UnstyledButton>
-						<UnstyledButton>À propos</UnstyledButton>
-
+						{liens}
 						<Group gap="lg" grow>
-							<Button variant="filled">Inscription</Button>
-							<Button variant="default">Connexion</Button>
+							{boutons}
 						</Group>
 					</Stack>
 				</AppShellAside>
