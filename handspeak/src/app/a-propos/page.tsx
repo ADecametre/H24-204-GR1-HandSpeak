2fa8f9ca43f7f04015a0a609058d2e674529f953
Martial <149718973+mazt1al@users.noreply.github.com>
2024-04-30 15:16:31 -0400

    Creation de la page à propos
    - Ajout de la section notre mission
    
    - Ajout de la section notre équipe
    
    - Ajout de la section FAQ
    
    - Ajout de la fonctionnalité Accordion pour rendre la partie FAQ plus belle à voir

---
+++ handspeak/src/app/a-propos/page.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import {
+	Container,
+	Flex,
+	Group,
+	Text,
+	Title,
+	Accordion,
+	AccordionControl,
+	AccordionItem,
+	AccordionPanel,
+} from "@mantine/core";
+import IconAPropos from "@/img/Apropos.png";
+import Image from "next/image";
+const questions = [
+	{
+		emoji: "❓",
+		value: "Est-ce que HandSpeak est gratuit?",
+		description: " Oui, HandSpeak est entierement gratuit ",
+	},
+	{
+		emoji: "❓",
+		value: "À qui s'adresse HandSpeak",
+		description:
+			"HandSpeak s'adresse à toute personne voulant apprendre le langage des signes américain (ASL)",
+	},
+	{
+		emoji: "❓",
+		value: "Quels sont les principaux avantages de HandSpeak ?",
+		description:
+			"HandSpeak propose une grande variété de ressources éducatives gratuites, y compris des leçons et des exercices interactifs, ce qui le rend accessible à tous les apprenants intéressés par l'ASL.",
+	},
+];
+const items = questions.map((item) => (
+	<AccordionItem key={item.value} value={item.value}>
+		<AccordionControl icon={item.emoji}>{item.value}</AccordionControl>
+		<AccordionPanel>{item.description}</AccordionPanel>
+	</AccordionItem>
+));
+
+export default function APropos() {
+	return (
+		<Group
+			className="bg-gray-100"
+			mih="calc(100dvh - 75px)"
+			justify="space-evenly"
+			py="md"
+		>
+			<Container size="sm" m={0}>
+				<Title order={1} mb="xl">
+					À propos de <span style={{ color: "#007bff" }}>HandSpeak</span>
+				</Title>
+				<Title order={2}> NOTRE MISSION </Title>
+				<Text pb={"sm"} mb="sm">
+					Notre mission est de rendre l&apos;apprentissage de la langue des
+					signes américaine (ASL) accessible à tous, en offrant un site web
+					interactif et intuitif. Nous croyons que la communication est un droit
+					fondamental, et notre objectif est d&apos;ouvrir les portes de la
+					communication pour les malentendants et ceux qui souhaitent apprendre
+					la langue des signes.
+				</Text>
+				<Title order={2}> NOTRE ÉQUIPE </Title>
+				<Flex direction="column" mb="sm">
+					<Text>Poumale, Martial Romarik</Text>
+					<Text>Brichi, Adam </Text>
+					<Text>Ibrahim, Adam Jihad </Text>
+					<Text>Merabet, Selmane Saïb </Text>
+				</Flex>
+				<Title order={2} mb="sm">
+					FAQ
+				</Title>
+				<Accordion variant="filled" defaultValue="Question 1">
+					{items}
+				</Accordion>
+			</Container>
+			<Image
+				src={IconAPropos}
+				className="w-[30vw] rounded-full" // largeur de l'image = 30% de la largeur de la page
+				alt="Globe"
+			/>
+		</Group>
+	);
+}
