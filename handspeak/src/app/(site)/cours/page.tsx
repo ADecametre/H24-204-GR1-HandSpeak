1cd6bbffd9ceee118960600bf74a099927c190da
Adam4718 <158094985+Adam4718@users.noreply.github.com>
2024-05-06 14:58:16 -0400

    Ajout d'un titre et d'une description de la page des cours
    Ajout d'un titre et d'une description de la page des cours.

--- handspeak/src/app/(site)/cours/page.tsx
+++ handspeak/src/app/(site)/cours/page.tsx
@@ -1,16 +1,10 @@
 //IMPORTS DE MANTINE (ne pas oublier d'installer le package du carousel)
-import { Card, Container, DEFAULT_THEME, Title } from "@mantine/core";
+import { Card, Container, DEFAULT_THEME, Title, Text } from "@mantine/core";
 import { Carousel, CarouselSlide } from "@mantine/carousel";
 import "@mantine/carousel/styles.css";
 import CarteCours, { data } from "@/components/CarteCours";
-
-//IMPORTS DE NEXT
-import type { Metadata } from "next";
-import { useMediaQuery } from "@mantine/hooks";
-
-export const metadata: Metadata = {
-	title: "Cours | HandSpeak",
-};
+import AnimationTitre from "@/components/AnimationTitre";
+import { IconSortAZ } from "@tabler/icons-react";
 
 export default function Home() {
 	//defini le contenu de chaque slide en utilisant les donnees des cartes (voir le data dans la composante CarteCours)
@@ -22,13 +16,41 @@ export default function Home() {
 
 	return (
 		<>
-			<Title pl={"lg"}>L&apos;ALPHABET DANS LA LANGUE DES SIGNES</Title>
+			<Title pl={"md"} pt={"md"}>
+				<Title c={"blue"} component="span">
+					<AnimationTitre></AnimationTitre>
+				</Title>
+				<br />
+				SELECTION DES COURS
+			</Title>
+			<Text pt={"md"} pl={"lg"} size="lg">
+				Donnez la parole à vos main avec{" "}
+				<Text
+					size="md"
+					fw={750}
+					variant="gradient"
+					gradient={{ from: "blue", to: "purple", deg: 90 }}
+					component="span"
+				>
+					HANDSPEAK!
+				</Text>
+			</Text>
+			<Text pt={"sm"} pl={"lg"} size="lg">
+				Accédez à notre vaste collection de cours sur cette page dès maintenant.
+				Bon apprentissage!
+			</Text>
+
+			<Title order={2} pl={"lg"} pt={"md"}>
+				L&apos;ALPHABET DANS LA LANGUE DES SIGNES
+			</Title>
 
 			<Carousel
 				slideSize={{ base: "100%", sm: "25%" }}
 				slideGap={{ base: "xl", sm: 2 }}
 				align="start"
 				slidesToScroll={4}
+				p={"lg"}
+				withControls={false}
 			>
 				{slides}
 			</Carousel>
