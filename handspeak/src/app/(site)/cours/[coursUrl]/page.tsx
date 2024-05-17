9991ba06f5d261d66908d636f265aadbc97843bf
norththinker <128328489+norththinker@users.noreply.github.com>
2024-05-17 19:35:16 -0400

    Ajout d'animations lorsque l'utilisateur complète une leçon
    1- Désactiver le bouton "Prochaine leçon" jusqu'à ce que l'utilisateur a complété la leçon actuelle.
    
    2- Animation du bouton "Prochaine Leçon" lorsque l'utilisateur complète la leçon actuelle.
    
    3- Ajout d'une animation d'un checkmark sur la caméra lorsque l'utilisateur réussit la leçon actuelle.

--- handspeak/src/app/(site)/cours/[coursUrl]/page.tsx
+++ handspeak/src/app/(site)/cours/[coursUrl]/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { IconCheck } from "@tabler/icons-react";
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { Category, GestureRecognizerOptions } from "@mediapipe/tasks-vision";
 import Camera from "@/components/Camera";
 import Main3D from "@/components/Main3D";
+// @ts-ignore
+import { Checkmark } from "react-checkmark";
 
 import {
 	Box,
@@ -22,9 +24,29 @@ import {
 export default function Demo() {
 	const [active, setActive] = useState(0);
 	const lessons = useMemo(() => ["A", "B", "C", "D", "E", "F"], []);
-	const [completedLessons, setCompletedLessons] = useState<string[]>([]);
+	const [completedLessons, setCompletedLessons] = useState<number>(0);
 
 	const [resultat, setResultat] = useState<Category>();
+
+	const [isButtonActive, setIsButtonActive] = useState(false);
+	const [showCheckmark, setShowCheckmark] = useState(false);
+
+	useEffect(() => {
+		if (
+			completedLessons > active ||
+			(resultat?.score && resultat.score >= 0.95)
+		) {
+			setIsButtonActive(true);
+			setShowCheckmark(true);
+		}
+		if (
+			resultat?.score &&
+			resultat.score >= 0.95 &&
+			active >= completedLessons
+		) {
+			setCompletedLessons((prev) => prev + 1);
+		}
+	}, [resultat, active, completedLessons, lessons]);
 
 	const options: GestureRecognizerOptions = useMemo(
 		() => ({
@@ -41,7 +63,8 @@ export default function Demo() {
 		setActive((current) =>
 			current < lessons.length - 1 ? current + 1 : current
 		);
-		setCompletedLessons((prev) => [...prev, lessons[active]]);
+		setIsButtonActive(false);
+		setShowCheckmark(false);
 	};
 	const prevStep = () =>
 		setActive((current) => (current > 0 ? current - 1 : current));
@@ -54,19 +77,13 @@ export default function Demo() {
 					Leçon {lessons[active]}
 				</Text>
 			</Flex>
-			<Group
-				justify="space-between"
-				align="center"
-				wrap="wrap"
-			>
+			<Group justify="space-between" align="center" wrap="wrap" grow>
 				<Timeline active={active} bulletSize={24} lineWidth={2}>
 					{lessons.map((lesson, index) => (
 						<Timeline.Item
 							key={index}
 							bullet={
-								completedLessons.includes(lesson) ? (
-									<IconCheck size={12} />
-								) : undefined
+								completedLessons > index ? <IconCheck size={12} /> : undefined
 							}
 							title={
 								<Text size="md" fw={500}>
@@ -80,28 +97,39 @@ export default function Demo() {
 					<Main3D letter={lessons[active].toLowerCase()} />
 				</Flex>
 				<Stack className="items-end">
-					<Box>
-					<Camera
-						options={options}
-						modelePath={`${process.cwd()}modeles/a-f.task`}
-						setResultat={setResultat}
-						className="min-w-[16dvw] min-h-[12dvw] sm:max-w-[20dvw]"
-					/>
-					<Text ta="center" size="lg" fw={800}>
-						&nbsp;{resultat?.categoryName}
-						<br />
-						&nbsp;
-						{resultat?.categoryName &&
-							Math.round((resultat?.score || 0) * 100) + " %"}
-					</Text>
+					<Box className="relative">
+						<Camera
+							options={options}
+							modelePath={`${process.cwd()}modeles/a-f.task`}
+							setResultat={setResultat}
+							className="min-w-[16dvw] min-h-[12dvw] sm:max-w-[20dvw]"
+						/>
+						{showCheckmark && (
+							<div className="absolute bottom-16 right-1">
+								<Checkmark size="large" color="#228be6" />
+							</div>
+						)}
+						<Text ta="center" size="lg" fw={800}>
+							&nbsp;{resultat?.categoryName}
+							<br />
+							&nbsp;
+							{resultat?.categoryName &&
+								Math.round(resultat.score * 100) + " %"}
+						</Text>
 					</Box>
 				</Stack>
 			</Group>
-			<Flex gap="xs" justify="center" className="w-full px-4 pb-4 pt-4">
+			<Flex gap="xl" justify="center" className="w-full px-4 pb-4 pt-4">
 				<Button variant="default" onClick={prevStep}>
 					Retour
 				</Button>
-				<Button onClick={nextStep}>Prochaine leçon</Button>
+				<Button
+					onClick={nextStep}
+					disabled={!isButtonActive}
+					className={`${isButtonActive ? "animate-polse" : ""} `}
+				>
+					Prochaine leçon
+				</Button>
 			</Flex>
 		</Container>
 	);
--- handspeak/tailwind.config.ts
+++ handspeak/tailwind.config.ts
@@ -1,20 +1,38 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			keyframes: {
+				polse: {
+					"0%": {
+						transform: "scale(1)",
+						boxShadow: "0 0 0 0 rgba(34,139,230,1)",
+					},
+					"70%": {
+						transform: "scale(1.2)",
+						boxShadow: "0 0 0 10px rgba(34,139,230,0)",
+					},
+					"100%": {
+						transform: "scale(1)",
+					},
+				},
+			},
+			animation: {
+				polse: "polse 2s infinite",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
