d61ca340f99d4798db2d0bea2039e993c1652519
norththinker <128328489+norththinker@users.noreply.github.com>
2024-07-19 23:12:02 -0400

    Ajout des images et de la progression à la page de leçons

--- handspeak/src/components/CarteCours.tsx
+++ handspeak/src/components/CarteCours.tsx
@@ -1,16 +1,17 @@
 import {
 	Card,
 	Image,
-	Text,
 	Badge,
 	Button,
 	Progress,
 	CardSection,
 	Title,
+	rem,
 } from "@mantine/core";
 import Link from "next/link";
 import NextImage from "next/image";
 import type db from "@/lib/db";
+import { IconAward } from "@tabler/icons-react";
 
 export default function CarteCours({
 	course,
@@ -19,30 +20,37 @@ export default function CarteCours({
 		ReturnType<typeof db.categories.getListeCours>
 	>[number]["courses"][number];
 }) {
+	const img = course.img;
+	const icon = <IconAward style={{ width: rem(12), height: rem(12) }} />;
 	const progression = course.progressions[0];
+
+	const lessonsDone = progression?.lessonsDone || 0;
+	const totalLessons = course.lessons.length;
+	const grade = progression?.grade || 0;
+
+	const progressValue = (lessonsDone / totalLessons) * 100;
+	const gradeValue = grade * 100;
 
 	return (
 		<Card shadow="sm" padding="lg" radius="md" withBorder mr={"md"}>
 			<CardSection>
 				<Image
 					component={NextImage}
-					src={course.img}
+					src={img}
 					alt={course.name}
 					sizes="50vw 100vw"
 					width={0}
 					height={0}
 				/>
-				<Progress radius="xs" value={0} />
+				<Progress radius="xs" value={progressValue} />
 			</CardSection>
-
 			<CardSection p={"sm"}>
 				<Title fw={500} order={3}>
 					{course.name}
 				</Title>
-				<Badge mt={"xs"}>{course.categoryId}</Badge>
-				<Text size="sm" c="dimmed" mt={"xs"}>
-					{course.description}
-				</Text>
+				<Badge rightSection={icon} mt={"xs"}>
+					{gradeValue}%
+				</Badge>
 			</CardSection>
 			<CardSection p={"xs"}>
 				<Button
